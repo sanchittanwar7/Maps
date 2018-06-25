aa846914ee3fee20d66f27244a03a6148119d7d7
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2018-06-25 16:03:32 +0530

    market cluster and test data added

--- imports/ui/Map1.js
+++ imports/ui/Map1.js
@@ -4,6 +4,9 @@ import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap'
 var jsonQuery = require('json-query')
 const countryData = require('../../countries.json')
 const cityData = require('../../cities.json')
+const data = require('../../data.json')
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+
 
 
 import L from 'leaflet'
@@ -16,7 +19,21 @@ import L from 'leaflet'
     const DEFAULT_VIEWPORT = {
       center: [51.505, -0.09],
       zoom: 13,
-    }   
+    }  
+
+
+    const MyPopupMarker = ({ children, position }) => (
+          <Marker position={position}>
+            <Popup>{children}</Popup>
+          </Marker>
+        )
+
+        const MyMarkersList = ({ markers }) => {
+          const items = markers.map(({ key, ...props }) => (
+            <MyPopupMarker key={key} {...props} />
+          ))
+          return <div style={{ display: 'none' }}>{items}</div>
+        } 
 
 export default class SimpleExample extends Component {
   state = {
@@ -25,7 +42,8 @@ export default class SimpleExample extends Component {
     zoom: 13,
     bounds: "outer",
     viewport: DEFAULT_VIEWPORT,
-    query: ""
+    query: "",
+    markers: null
   }
 
   modifyString() {
@@ -40,75 +58,73 @@ export default class SimpleExample extends Component {
 
   getCoordinates() {
     console.log('here')
-    var lat = 0, lng = 0, zoom = 0
-    var data = {
-        people: countryData
-    }
-    var searchString = this.modifyString()
-    // var searchString = this.state.query.charAt(0).toUpperCase() + this.state.query.slice(1)
-    let ans = jsonQuery(`people[name=${searchString}]`, {
-      data: data
-    })
-    // console.log(ans.parents[2].value.latitude)
-    console.log(ans)
-    if(ans.key === null){
-        data = {
-            people: cityData
-        }
-        ans = jsonQuery(`people[FIELD1=${searchString}]`, {
-            data: data
-        })
-        console.log('city')
-        console.log(ans)
-        lat = ans.value.FIELD3
-        lng = ans.value.FIELD4
-
-    }
-    else{
-        lat = ans.value.latitude
-        lng = ans.value.longitude
-        zoom = ans.value.zoom
-        console.log(zoom)
-    }
-    console.log('latitude' + lat)
-    console.log('longitude' + lng)
-    console.log('zoom' + zoom)
-
-    let center = []
-    center.push(lat)
-    center.push(lng)
-    if(zoom === 0)
-        zoom = 10
-    let viewport = {
-        center: center,
-        zoom: zoom
-    }
-    console.log(viewport)
-    this.setState({viewport})
-
-    // let URL = `http://api.geonames.org/searchJSON?q=${this.state.query}&maxRows=1&username=sanchittanwar7`
-    // fetch(URL, {
-    //     method: 'GET'
+    // var lat = 0, lng = 0, zoom = 0
+    // var data = {
+    //     people: countryData
+    // }
+    // var searchString = this.modifyString()
+    // // var searchString = this.state.query.charAt(0).toUpperCase() + this.state.query.slice(1)
+    // let ans = jsonQuery(`people[name=${searchString}]`, {
+    //   data: data
     // })
-    // .then(response => response.json())
-    // .then(json => {
-    //     console.log(json);
-    //     let center = [], zoom
-    //     center.push(parseFloat(json.geonames[0].lat))
-    //     center.push(parseFloat(json.geonames[0].lng))
-    //     if(json.geonames[0].fcl === 'A')
-    //         zoom = 4
-    //     else
-    //         zoom = 10
-    //     let viewport = {
-    //         center: center,
-    //         zoom: zoom
+    // // console.log(ans.parents[2].value.latitude)
+    // console.log(ans)
+    // if(ans.key === null){
+    //     data = {
+    //         people: cityData
     //     }
-    //     console.log(viewport)
-    //     this.setState({viewport})
-    //     // console.log('artist', artist);
-    //     // this.setState({artist, stats: artist.stats, bio: artist.bio, images: artist.image});
-    // });
+    //     ans = jsonQuery(`people[FIELD1=${searchString}]`, {
+    //         data: data
+    //     })
+    //     console.log('city')
+    //     console.log(ans)
+    //     lat = ans.value.FIELD3
+    //     lng = ans.value.FIELD4
+
+    // }
+    // else{
+    //     lat = ans.value.latitude
+    //     lng = ans.value.longitude
+    //     zoom = ans.value.zoom
+    //     console.log(zoom)
+    // }
+    // console.log('latitude' + lat)
+    // console.log('longitude' + lng)
+    // console.log('zoom' + zoom)
+
+    // let center = []
+    // center.push(lat)
+    // center.push(lng)
+    // if(zoom === 0)
+    //     zoom = 10
+    // let viewport = {
+    //     center: center,
+    //     zoom: zoom
+    // }
+    // console.log(viewport)
+    // this.setState({viewport})
+
+    let URL = `http://api.geonames.org/searchJSON?q=${this.state.query}&maxRows=1&username=sanchittanwar7`
+    fetch(URL, {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+        let center = [], zoom
+        center.push(parseFloat(json.geonames[0].lat))
+        center.push(parseFloat(json.geonames[0].lng))
+        if(json.geonames[0].fcl === 'A')
+            zoom = 4
+        else
+            zoom = 10
+        let viewport = {
+            center: center,
+            zoom: zoom
+        }
+        console.log(viewport)
+        this.setState({viewport})
+    });
   }
 
       onClickInner = () => {
@@ -126,6 +142,30 @@ export default class SimpleExample extends Component {
       onViewportChanged = viewport => {
         console.log(viewport)
         this.setState({ viewport })
+      }
+
+      getMarkers(){
+        console.log(data)
+        var markers = []
+        var marker = {}, point = []
+        data.map((tweet, k) => {
+            marker = {}
+            point = []
+            marker.key = k
+            point.push(tweet.lat)
+            point.push(tweet.lng)
+            marker.position = point
+            marker.children = `${k}th popup`
+            markers.push(marker)
+        })
+        console.log(markers)
+        this.setState({markers})
+      }
+
+
+      componentWillMount() {
+            this.getMarkers()
+
       }
 
 
@@ -244,11 +284,19 @@ export default class SimpleExample extends Component {
             this.setState({ viewport })
           }
         }
-        viewport={this.state.viewport}>
+        viewport={this.state.viewport}
+      >
         <TileLayer
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <MarkerClusterGroup>
+
+        <MyMarkersList markers={this.state.markers} />
+
+        </MarkerClusterGroup>
+
+
         <Circle center={center} fillColor="blue" radius={200} />
         <Polygon color="purple" positions={polygon} />
         <Marker position={position}>
